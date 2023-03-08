324476a59799044a7142b504e9495912e20f47a5
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-03-08 18:03:20 -0500

    early developments

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-const panel = document.getElementById("panel");
+document.getElementById('select-left').addEventListener('click', () => document.body.dataset.side = 'left');
+document.getElementById('select-right').addEventListener('click', () => document.body.dataset.side = 'right');
 
-for (let i = 0; i < 6; i++) {
-	const item = document.createElement("div");
-	item.classList.add("empty");
-	item.classList.add(i.toString());
-	panel.append(item);
+
+const COLORS = ['G','B','Y','R','P']
+const VALID_COLORS = ['G','B','Y','R']
+
+function pull_lever(id) {
+	const row = document.querySelector('.colors');
+	const item = row?.children[id];
+	const color = item?.dataset.color;
+
+	const index = COLORS.indexOf(color);
+	const new_index = ((index ? index > -1 : 0) + 1) % COLORS.length;
+
+	item.dataset.color = COLORS[new_index];
 }
