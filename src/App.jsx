eb55548445c4cfe5be65f0a0033545118a7cd5c3
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-03-21 17:25:54 -0400

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,71 @@
 import { useState } from 'react';
 import './App.css';
 
-function Light({ value }) {
-	return <div class="light" data-value={value} />
+
+const ORDER_RIGHT = [1, 4, 0, 5, 3, 2];
+const ORDER_LEFT = [3, 2, 0, 5, 1, 4];
+
+function Puzzle({ is_right = true }) {
+	let [state, setState] = useState([-1, -1, -1, -1, -1, -1]);
+
+	const order = is_right ? ORDER_RIGHT : ORDER_LEFT;
+
+	function pull_lever(index) {
+		const destination = order[index];
+		return () => {
+			setState(state.map((v, i) => i == destination ? (v + 1) % 5 : v));
+		}
+	}
+
+	return (
+		<div className="puzzle">
+			<div className="signets">
+				{state.map((state) => <div className="signet" data-value={state}></div>)}
+			</div>
+			<div className="levers">
+				<div className="lever-group">
+					<div className="lever" onClick={pull_lever(0)} />
+					<div className="lever" onClick={pull_lever(1)} />
+				</div>
+				<div className="lever-group">
+					<div className="lever" onClick={pull_lever(2)} />
+					<div className="lever" onClick={pull_lever(3)} />
+				</div>
+				<div className="lever-group">
+					<div className="lever" onClick={pull_lever(4)} />
+					<div className="lever" onClick={pull_lever(5)} />
+				</div>
+			</div>
+			<div className="target">
+
+			</div>
+		</div>
+	)
 }
+
+
+
+
+
+
+
 
 function App() {
 
-	const [lights, setLights] = useState([null, null, null, null, null, null]);
+	let [is_right, setRight] = useState(true)
+
 
 	return (
 		<>
-			<div>Hello World</div>
+			<div className="options">
+				<div className={!is_right && "selected"}>
+					Left
+				</div>
+				<div className={is_right && "selected"}>
+		Right
+				</div>
+			</div>
+			<Puzzle is_right={is_right} />
 		</>
 	);
 }
